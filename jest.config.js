526dbe0a1f7ecad18b7abeeb6f8a74d287e43452
Ville Venäläinen <ville@mindhive.fi>
2019-03-14 02:40:37 +0200

    fix: optimized-package-publishing-size
    Optimized the list of the files to be published in the package

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/__specs__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
 };
